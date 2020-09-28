73603b6d16d39b8e617eb7b0d396687aa672acc9
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2020-09-27 21:02:38 -0500

    using blur event to know the field validity/value

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -22,6 +22,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
   }
 
   onSubmit(form: NgForm) {
